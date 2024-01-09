b0a7b473a1187ba33072d8ad5ae65b670dbeaf35
KeddyB <bokelusi@gmail.com>
2024-01-09 13:30:01 +0100

    this is the first commit of the day

--- app/index.js
+++ app/index.js
@@ -31,7 +31,7 @@ const Home = () => {
                 >
                     <Welcome />
                     <Popularjobs />
-                    <Popularjobs />
+                    <Nearbyjobs />
                 </View>
             </ScrollView>
         </SafeAreaView>
