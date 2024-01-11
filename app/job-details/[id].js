0fa275a043c49f40575402225e7caff37a1da2f1
KeddyB <bokelusi@gmail.com>
2024-01-11 10:54:30 +0100

    commit

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,4 +1,4 @@
-import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react'
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native'
 import { Stack, useRouter, useSearchParams } from 'expo-router'
 import { useCallback, useState } from 'react'
 
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -32,8 +32,8 @@ const NearbyJobs = () => {
           data?.map((job) => (
             <NearbyJobCard
               job={job}
-              key={`nearby-job-${job?.job_id}`}
-              handleNavigation={()=> router.push(`/job-details/${job.job_id}`)}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
           ))
         )}
