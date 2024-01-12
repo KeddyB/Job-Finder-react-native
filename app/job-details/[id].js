ab2d3da0fcb237d0c8793d3f9ff790c5de2d8ddb
KeddyB <bokelusi@gmail.com>
2024-01-12 13:57:41 +0100

    this is a commit

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -78,7 +78,9 @@ const JobDetails = () => {
             </View>
           )}
         </ScrollView>
+        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'}/>
       </>
+      
     </SafeAreaView>
   )
 }
